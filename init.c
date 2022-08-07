ffd330eb033211c46d1c85a90d6c6270763fc68f
Vincent Yip <vincentyip@Vincents-MBP.fios-router.home>
2022-08-07 12:47:13 -0400

    ranks and files brd

--- init.c
+++ init.c
@@ -18,6 +18,30 @@ U64 PieceKeys[13][120];
 U64 SideKey;
 U64 CastleKeys[16];
 
+int FilesBrd[BRD_SQ_NUM];
+int RanksBrd[BRD_SQ_NUM];
+
+void InitFilesRanksBoard(){
+	int index = 0;
+	int file = FILE_A;
+	int rank = RANK_1;
+	int sq = A1;
+	int sq64 = 0;
+
+	for(index = 0; index < BRD_SQ_NUM; index ++){
+		FilesBrd[index] = OFFBOARD;
+		RanksBrd[index] = OFFBOARD;
+	}
+
+	for(rank = RANK_1; rank <= RANK_8; rank++){
+		for(file = FILE_A; file <= FILE_H; file ++){
+			sq = FR2SQ(rank, file);
+			FilesBrd[sq] = file;
+			RanksBrd[sq] = rank;
+		}
+	}
+}
+
 void InitHashKeys() {
 	
 	int index = 0;
@@ -77,4 +101,5 @@ void AllInit() {
 	InitSq120To64();	
 	InitBitMasks();
 	InitHashKeys();
+	InitFilesRanksBoard();
 }
